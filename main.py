c320f7a553d88131a49fda22836cbe1bc0174f2a
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-01-09 11:43:29 +0000

    feat:data ingestion pipeline added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_1_data_ingestion_pipeline import DataIngestionPipeline
 
-logger.info("hi")
+
+# logger.info("hi")
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+try:
+    logger.info(f"=======Stage {STAGE_NAME} started=======")
+    pipe = DataIngestionPipeline()
+    pipe.main()
+    logger.info(f"======== STAGE : {STAGE_NAME} is completed=======")
+except Exception as e:
+    logger.info(e)
+    raise(e)
