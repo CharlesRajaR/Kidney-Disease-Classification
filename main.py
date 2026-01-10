526de8f26066278df9e5b0ad3600b44e22e1a37d
Charles Raja R <rcr2003rcharlesraja@gmail.com>
2026-01-10 11:27:20 +0000

    feat: initialize VGG16 base model for transfer learning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_1_data_ingestion_pipeline import DataIngestionPipeline
+from cnnClassifier.pipeline.stage_2_prepare_base_model_pipeline import PrepareBaseModelPipeline
 
 
 # logger.info("hi")
@@ -14,3 +15,15 @@ try:
 except Exception as e:
     logger.info(e)
     raise(e)
+
+
+STAGE_NAME = "PREPARE BASE MODEL STAGE"
+
+try:
+    logger.info(f"========Stage {STAGE_NAME} started ==========")
+    pipe = PrepareBaseModelPipeline()
+    pipe.main()
+    logger.info(f"========== STAGE : {STAGE_NAME} is completed ============")
+except Exception as e:
+    logger.info(e)
+    raise e
